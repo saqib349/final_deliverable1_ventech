b0509655069f46583b82fdacff6b3d62fd9d6469
saqib349 <saqib.akber624@gmail.com>
2026-09-09 13:40:54 +0500

    added delete user for admin

--- project/src/app/services/user-service.ts
+++ project/src/app/services/user-service.ts
@@ -11,6 +11,6 @@ export class UserService {
         return this.http.get<{data:Admin_users[]}>("https://final-deliverable1-ventech-o7q6.vercel.app/admin/user")
     }
     deleteUser(_id:string){
-        return this.http.delete<{data:Admin_users}>(`http://localhost:8000/admin/user/${_id}`)
+        return this.http.delete<{data:Admin_users}>(`https://final-deliverable1-ventech-o7q6.vercel.app/admin/user/${_id}`)
     }
 }
