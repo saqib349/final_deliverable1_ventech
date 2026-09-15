a6ce9f376eee3f980beb4371a011dbe90aaf832e
saqib349 <saqib.akber624@gmail.com>
2026-09-15 15:17:32 +0500

    added confirm password

--- project/src/app/pages/signup/signup.ts
+++ project/src/app/pages/signup/signup.ts
@@ -1,7 +1,32 @@
+
 import { Component, inject, signal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
+
+
+export const passwordMatchValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+
+  const password = control.get('password')?.value;
+  const confirmPassword = control.get('Cpassword')?.value;
+
+  if (password !== confirmPassword) {
+    return { passwordMismatch: true };
+  }
+
+  return null;
+};
+
 
 @Component({
   selector: 'app-signup',
@@ -10,56 +35,78 @@ import { AuthService } from '../../services/auth-service';
   styleUrl: './signup.css',
 })
 export class Signup {
-  router = inject(Router)
-  errorMessage = ""
-  showPassword=signal(false)
-  authService=inject(AuthService)
- 
-  signupForm = new FormGroup({
-    username: new FormControl<string>('', {
-      nonNullable: true,
-      validators: [
-        Validators.required,
-        Validators.maxLength(10)
-      ]
+
+  router = inject(Router);
+  authService = inject(AuthService);
+
+  errorMessage = "";
+
+  showPassword = signal(false);
+
+  signupForm = new FormGroup(
+    {
+      username: new FormControl<string>('', {
+        nonNullable: true,
+        validators: [
+          Validators.required,
+          Validators.maxLength(10)
+        ]
+      }),
+
+      email: new FormControl<string>('', {
+        nonNullable: true,
+        validators: [
+          Validators.required,
+          Validators.email
+        ]
+      }),
+
+      password: new FormControl<string>('', {
+        nonNullable: true,
+        validators: [
+          Validators.required,
+          Validators.minLength(8)
+        ]
+      }),
+
+      Cpassword: new FormControl<string>('', {
+        nonNullable: true,
+        validators: [
+          Validators.required,
+          Validators.minLength(8)
+        ]
+      })
+    },
+    {
+      validators: passwordMatchValidator
     }
-    ),
-    email: new FormControl<string>('', {
-      nonNullable: true,
-      validators: [
-        Validators.required,
-        Validators.email
-      ]
-    }),
-    password: new FormControl<string>('', {
-      nonNullable: true,
-      validators: [
-        Validators.required,
-        Validators.minLength(8)
-      ]
-    })
-  })
+  );
+
 
   handleSignup() {
-      const user:Omit<User, "role">={
-        username:this.signupForm.controls.username.value,
-        email:this.signupForm.controls.email.value,
-        password:this.signupForm.controls.password.value
-      }
-      this.authService.signupUser(user).subscribe({
-        next:(result)=>{
-          console.log(result)
-          this.router.navigate(['/login'])
-        } 
-        ,
-        error:(err)=>{
-          this.errorMessage=err.error.message
-        }
-      })
 
+    const user: Omit<User, "role"> = {
+      username: this.signupForm.controls.username.value,
+      email: this.signupForm.controls.email.value,
+      password: this.signupForm.controls.password.value
+    };
+
+    this.authService.signupUser(user).subscribe({
+      next: (result) => {
+        console.log(result);
+        this.router.navigate(['/login']);
+      },
+
+      error: (err) => {
+        this.errorMessage = err.error.message;
+      }
+    });
   }
+
+
   togglePassword() {
     this.showPassword.update(value => !value);
   }
 
 }
+
