d5495e379953688f190836f73e19f48a44064637
saqib349 <saqib.akber624@gmail.com>
2026-09-16 14:27:51 +0500

    fix userdetail issue

--- project/src/app/services/user-service.ts
+++ project/src/app/services/user-service.ts
@@ -17,7 +17,7 @@ export class UserService {
         return this.http.patch<{data:Admin_users}>(`https://final-deliverable1-ventech-o7q6.vercel.app/admin/user/${user._id}`,user)
     }
     getUserById(_id:string | null){
-        return this.http.get<{data:Admin_users}>(`http://localhost:8000/admin/user/${_id}`)
+        return this.http.get<{data:Admin_users}>(`https://final-deliverable1-ventech-o7q6.vercel.app/admin/user/${_id}`)
     }
 
 }
